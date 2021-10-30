286e25e1b64144366cba7b940d0a37919dbc5b04
pikcars <44181174+pikcars@users.noreply.github.com>
2021-10-30 05:02:25 +0100

    new

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -124,7 +124,7 @@ class AuthController extends Controller
           return response()->json(["status"=>"success","message"=>"user created"]);
          }
               
-
+        //id_rsa =*=*c!uZTgRd%O8cQR
 
      }
 
