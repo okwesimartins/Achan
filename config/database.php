71b272a4ec8879d4c02715d8c4c6df0f11d57522
pikcars <44181174+pikcars@users.noreply.github.com>
2021-10-30 06:31:16 +0100

    new

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-$DATABASE_URL =parse_url('postgres://rmhffnqguqtxxt:7a67cf24b0610753a907e7d41d2ce4e0be2765fefed1228ed2dc45494066d1a7@ec2-44-198-196-169.compute-1.amazonaws.com:5432/d9fgqas9pjrul2');
+$DATABASE_URL =parse_url('');
 return [
 
     /*
@@ -67,7 +67,7 @@ return [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
             'host' => $DATABASE_URL["host"],
-            'port' => env('DB_PORT', '5432'),
+            'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["pass"],
