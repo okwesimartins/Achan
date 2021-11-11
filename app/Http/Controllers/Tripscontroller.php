97739639d5f8baf433f212f965cb08f32f3316a9
pikcars <44181174+pikcars@users.noreply.github.com>
2021-11-11 02:32:16 +0100

    new

---
+++ app/Http/Controllers/Tripscontroller.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Trips;
+use App\Models\Admin;
+use App\Models\User;
+use Carbon\Carbon;
+class Tripscontroller extends Controller
+{
+    public function admintrips(){
+        $branch = auth()->guard('admin-api')->user()->branches()->get();
+        $carbondate=Carbon::now();
+        $year=$carbondate->year;
+        $tripsarray = array();
+        foreach($branch as $value){
+            $userid= $value->userid;
+            $tripcheck = Trips::where('airline_branch_id',$userid)->where('year',$year)->latest()->get();
+
+            if(empty($tripcheck)){
+               return response()->json(["status"=>"failed","message"=>"No trips yet"]);
+            }else{
+
+                foreach($tripcheck as $value1){
+                    $trip= [
+                        "customer_name"=> $value1->passenger_name,
+                        "location"=> $value1->trip_from,
+                        "destination"=>$value1->dest_area,
+                        "booking_date"=>$value1->booking_date,
+                        "booking_time"=>$value1->booking_time,
+                        "status"=>$value1->status
+                    ];
+                    array_push($tripsarray, $trip);
+                }
+            
+            }
+
+
+        }
+
+         return response()->json($tripsarray);
+
+    }
+
+    //completed trips for admin
+
+    public function admincompletedtrip(){
+           
+        $branch = auth()->guard('admin-api')->user()->branches()->get();
+        $carbondate=Carbon::now();
+        $year=$carbondate->year;
+        $tripsarray = array();
+        foreach($branch as $value){
+            $userid= $value->userid;
+            $complete="Complete";
+            $tripcheck = Trips::where('airline_branch_id',$userid)->where('year',$year)->where('status', $complete)->get();
+          
+           
+
+                foreach($tripcheck as $value1){
+                    $trip= [
+                        "customer_name"=> $value1->passenger_name,
+                        "location"=> $value1->trip_from,
+                        "destination"=>$value1->dest_area,
+                        "booking_date"=>$value1->booking_date,
+                        "booking_time"=>$value1->booking_time,
+                        "status"=>$value1->status
+                    ];
+                    array_push($tripsarray, $trip);
+                }
+            
+            
+
+
+        }
+
+         return response()->json($tripsarray);
+
+    }
+
+// admin pending trip
+public function adminpendingtrip(){
+           
+    $branch = auth()->guard('admin-api')->user()->branches()->get();
+    $carbondate=Carbon::now();
+    $year=$carbondate->year;
+    $tripsarray = array();
+    foreach($branch as $value){
+        $userid= $value->userid;
+        $tripcheck = Trips::where('airline_branch_id',$userid)->where('year',$year)->where('status',"pending")->get();
+
+   
+
+            foreach($tripcheck as $value1){
+                $trip= [
+                    "customer_name"=> $value1->passenger_name,
+                    "location"=> $value1->trip_from,
+                    "destination"=>$value1->dest_area,
+                    "booking_date"=>$value1->booking_date,
+                    "booking_time"=>$value1->booking_time,
+                    "status"=>$value1->status
+                ];
+                array_push($tripsarray, $trip);
+            }
+        
+        
+
+
+    }
+
+     return response()->json($tripsarray);
+
+}
+
+// admin cancelled trips
+
+ public function cancelledtrips(){
+    $branch = auth()->guard('admin-api')->user()->branches()->get();
+    $carbondate=Carbon::now();
+    $year=$carbondate->year;
+    $tripsarray = array();
+    foreach($branch as $value){
+        $userid= $value->userid;
+        $tripcheck = Trips::where('airline_branch_id',$userid)->where('year',$year)->where('status',"cancelled")->get();
+        
+        
+      
+            foreach($tripcheck as $value1){
+                $trip= [
+                    "customer_name"=> $value1->passenger_name,
+                    "location"=> $value1->trip_from,
+                    "destination"=>$value1->dest_area,
+                    "booking_date"=>$value1->booking_date,
+                    "booking_time"=>$value1->booking_time,
+                    "status"=>$value1->status
+                ];
+                array_push($tripsarray, $trip);
+            }
+        
+        
+
+
+    }
+
+     return response()->json($tripsarray);
+ }
+}
--- routes/api/admin.php
+++ routes/api/admin.php
@@ -5,12 +5,23 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Branchcontroller;
 use App\Http\Controllers\Graphcontroller;
+
+use App\Http\Controllers\Tripscontroller;
+
 Route::post('admin/login',[AuthController::class, 'adminLogin']);
 Route::post('admin/register',[AuthController::class, 'register']);
 Route::group( ['prefix' => 'admin','middleware' => ['auth:admin-api','scopes:admin'] ],function(){
     // authenticated admin routes here 
      Route::get('admin_graph',[Graphcontroller::class, 'graph']);
      Route::get('dashboardapi',[AuthController::class, 'dashboardapi']);
+     Route::get('all_trips',[Tripscontroller::class, 'admintrips']);
+
+     Route::get('admincompleted',[Tripscontroller::class, 'admincompletedtrip']);
+
+     Route::get('adminpending',[Tripscontroller::class, 'adminpendingtrip']);
+
+     Route::get('admincancelled',[Tripscontroller::class, 'cancelledtrips']);
+
      Route::post('create_branches',[AuthController::class, 'createbranch']);
 
      Route::get('admingraphcom',[Graphcontroller::class, 'admincomgraph']);  
