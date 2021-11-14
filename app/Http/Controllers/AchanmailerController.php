e2bf3db7d8b390625f8a95a353d325ca97b6e8d7
pikcars <44181174+pikcars@users.noreply.github.com>
2021-11-14 04:22:16 +0100

    new

--- app/Http/Controllers/AchanmailerController.php
+++ app/Http/Controllers/AchanmailerController.php
@@ -20,20 +20,20 @@ class AchanmailerController extends Controller
         if(empty($admin)){
            return response()->json(["status"=>"failed","message"=>"Email does not exist only admin can change password"]);
         }else{
-            
+
             $title= 'Change password';
             $ademail=$admin->email;
 
              $airline_details=[
-            'airline_name'=> $admin->name,
-            'email'=> $ademail
+            "airline_name"=> $admin->name,
+            "email"=> $ademail
             ];
        
             $otpgen= mt_rand(100000,999999);
             $str= (string)$otpgen;
             
             $otp=[
-                'otp'=>$str,
+                "otp"=>$str,
             ];
             
            $sendmail= Mail::to( $airline_details['email'])->send(new SendMail($title,$airline_details,$otp));
