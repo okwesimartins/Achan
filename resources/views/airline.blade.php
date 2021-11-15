de94a1dc6de06d2e69640d97c9d95d7d3905a59c
pikcars <44181174+pikcars@users.noreply.github.com>
2021-11-16 00:06:45 +0100

    new

--- resources/views/airline.blade.php
+++ resources/views/airline.blade.php
@@ -1,6 +1,6 @@
 <br/>
   
-<img src="{{assets('img/acicon.png')}}" style="width:30%" alt="App Logo"/>
+<img src="{{assets('img/acicon.png')}}"  alt="App Logo"/>
 Dear management of {{ $airline_details['airline_name'] }}
 
 <br/><br/>
