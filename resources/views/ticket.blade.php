f6c9976f28b5298383011a4b9ab4804f1304aa2f
pikcars <44181174+pikcars@users.noreply.github.com>
2021-12-22 22:18:11 +0100

    new

--- resources/views/ticket.blade.php
+++ resources/views/ticket.blade.php
@@ -2,57 +2,62 @@
 
        
      
-       Ticket for first trip:
+       <h2 >Ticket for first trip:</h2>
             <br/>
       <img src="{{ asset('img/acicon.png') }}" style="max-width:100px;max-height:90px" alt="App Logo"/>
        <br/>
        Hello {{ $user_details['name'] }} thank you for choosing achantaxi, please click the buttons below to view your tickets
       
         <br/><br/>
-       <button  style="outline:none;width:100px;color:white"> <a href="{{ $ticket_link['link1'] }}"> View ticket</a></button>
+       <button  style="outline:none;width:100px;height:50px;border-radius:5px"> <a href="{{ $ticket_link['link1'] }}" style="color:white"> View ticket</a></button>
        <br/>
-       Ticket for second trip:
+       <h2 style="margin-top:4px">Ticket for second trip:</h2>
             <br/>
             <img src="{{ asset('img/acicon.png') }}" style="max-width:100px;max-height:90px" alt="App Logo"/>
        <br/>
        Hello {{ $user_details['name'] }}  thank you for choosing achantaxi, please click the buttons below to view your tickets
         <br/><br/>
-       <button  style="outline: none;width:100px;height:50px;"> <a href="{{ $ticket_link['link2'] }}" style="color:white">View ticket</a> </button>
+       <button  style="outline: none;width:100px;height:50px;border-radius:5px"> <a href="{{ $ticket_link['link2'] }}" style="color:white">View ticket</a> </button>
 
        <br/>
-       Ticket for third trip:
+       <h2 style="margin-top:4px">Ticket for third trip:</h2>
             <br/>
             <img src="{{ asset('img/acicon.png') }}" style="max-width:100px;max-height:90px" alt="App Logo"/>
        <br/>
        Hello {{ $user_details['name'] }}  thank you for choosing achantaxi, please click the buttons below to view your tickets
         <br/><br/>
-       <button  style="outline:none;width:100px;color:white"> <a href="{{ $ticket_link['link3'] }}"> View ticket</a></button>
+       <button  style="outline:none;width:100px;height:50px;border-radius:5px"> <a href="{{ $ticket_link['link3'] }}" style="color:white"> View ticket</a></button>
        <br/>
-       Ticket for forth trip:
+       <h2 style="margin-top:4px">Ticket for forth trip:</h2>
             <br/>
             <img src="{{ asset('img/acicon.png') }}" style="max-width:100px;max-height:90px" alt="App Logo"/>
        <br/>
        Hello {{ $user_details['name'] }} , thank you for choosing achantaxi, please click the buttons below to view your tickets
         <br/><br/>
-       <button  style="outline: none;width:100px;color:white"> <a href="{{ $ticket_link['link4'] }}">View ticket</a> </button>
+       <button  style="outline: none;width:100px;height:50px;border-radius:5px"> <a href="{{ $ticket_link['link4'] }}" style="color:white">View ticket</a> </button>
        
 
 
 @elseif(array_key_exists("link1", $ticket_link) && array_key_exists("link2", $ticket_link))
 
        
-<br/>
+
+ Ticket for first trip:
+      <br/>
 <img src="{{ asset('img/acicon.png') }}" style="max-width:100px;max-height:90px" alt="App Logo"/>
  <br/>
- Hello {{ $user_details['name'] }} , thank you for choosing achantaxi, please click the buttons below to view your tickets
- <br/><br/>
- Ticket for first trip:
+ Hello {{ $user_details['name'] }}  thank you for choosing achantaxi, please click the buttons below to view your tickets
+ 
   <br/><br/>
- <button  style="outline:none;background-color:#0646A2;color:white"> <a href="{{ $ticket_link['link1'] }}"> View ticket</a></button>
+ <button  style="outline:none;width:100px;height:50px;border-radius:5px"> <a href="{{ $ticket_link['link1'] }}" style="color:white"> View ticket</a></button>
  <br/>
  Ticket for second trip:
-  <br/><br/>
- <button  style="outline: none;background-color:#0646A2;color:white"> <a href="{{ $ticket_link['link2'] }}">View ticket</a> </button>
+      <br/>
+<img src="{{ asset('img/acicon.png') }}" style="max-width:100px;max-height:90px" alt="App Logo"/>
+ <br/>
+ Hello {{ $user_details['name'] }} thank you for choosing achantaxi, please click the buttons below to view your tickets
+ <br/><br/>
+ <button  style="outline: none;width:100px;height:50px;border-radius:5px"> <a href="{{ $ticket_link['link2'] }}"  style="color:white">View ticket</a> </button>
 
 @else
       <br/>
